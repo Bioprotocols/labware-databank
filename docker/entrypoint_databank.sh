a4f05b4ff57c8ccf565764194ba04584710c755a
mark doerr <mark.doerr@uni-greifswald.de>
2023-03-15 14:26:15 +0100

    fix(entrypoint_databank.sh): uvcorn server

--- docker/entrypoint_databank.sh
+++ docker/entrypoint_databank.sh
@@ -3,7 +3,14 @@
 
 # Labop jupyter notebook entrypoint script
 
-rdflib-endpoint serve --host 0.0.0.0 --port 8000 *.ttl
+echo "Starting databank uvicorn server"
+# rdflib-endpoint serve --host 0.0.0.0 --port 8000 *.ttl &
+cd /opt/labwaredb
+echo "workdir:"
+ls -Al
+echo "Starting databank uvicorn server"
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+
 # if [$SILA_SERVER == "true"]
 if [ "$SILA_SERVER" = "true" ]; then
     echo "Starting SiLA server"
@@ -12,4 +19,4 @@ fi
 
 echo "RDFlib endpoint started"
 echo "Starting SiLA server .... --host 0.0.0.0  @port 50052"
-labop_labware_sila --insecure --port 50052
+labop_labware_sila --insecure --port 50052 &
