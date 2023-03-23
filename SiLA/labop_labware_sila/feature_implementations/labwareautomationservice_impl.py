160632a72fc11335ff0519331a839847bd0c03ca
mark doerr <mark.doerr@uni-greifswald.de>
2023-03-23 19:54:59 +0100

    feat: labware queries added to SiLA interface

--- SiLA/labop_labware_sila/feature_implementations/labwareautomationservice_impl.py
+++ SiLA/labop_labware_sila/feature_implementations/labwareautomationservice_impl.py
@@ -14,6 +14,8 @@ from ..generated.labwareautomationservice import (
     LabwareAutomationServiceBase,
 )
 
+from labop_labware_ontology.labop_labware_ontology_impl import LabwareInterface
+
 if TYPE_CHECKING:
     from ..server import Server
 
@@ -22,16 +24,28 @@ class LabwareAutomationServiceImpl(LabwareAutomationServiceBase):
     def __init__(self, parent_server: Server) -> None:
         super().__init__(parent_server=parent_server)
 
+        # load all ontologies
+        self.labware_ontology = LabwareInterface()
+        self.lw_abox = self.labware_ontology.lolw_abox.lolwa
+
+
     def GetLabwareDimensions(
         self, Manufacturer: str, ProductID: str, Unit: str, *, metadata: MetadataDict
     ) -> GetLabwareDimensions_Responses:
         
         logging.debug(f"lw dimensions: Manufacturer: {Manufacturer}, productID: {ProductID}, unit: {Unit}")
 
-        lw_dimensions = { "Length": 127.0, "Width": 85.0, "Height": 42.0 }
+        try:
+            res = self.lw_abox.search(hasManifacturer = Manufacturer, hasProductID = ProductID)
+        except Exception as e:
+            logging.error(f"Error: {e}")
+            # TODO: raise error
+            return GetLabwareDimensions_Responses(Dimensions="{}")
+
+        lw_dimensions = { "Length": res.hasLength.length, "Width": res.hasWidth.width, "Height": res.hasHeight.height }
 
         return GetLabwareDimensions_Responses(Dimensions=str(lw_dimensions))
-
+        
 
     def GetGrippingHeight(
         self, Manufacturer: str, ProductID: str, Unit: str, Lidded: bool, *, metadata: MetadataDict
